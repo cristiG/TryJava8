3ca56a479d7b16e1c922af697e357e6e56249cf1
cristiG <cristian.gherghinescu@gmail.com>
2014-09-23 08:37:36 +0300

    Added java in javascript example
    Run it with jjs.exe -fx javaFXApp.js

---
+++ JavaFXIntro/src/javafxintro/javaFXApp.js
@@ -0,0 +1,5 @@
+
+var message = new javafx.scene.control.Label("Javascript - Java : Ham - Hamster");
+message.font = new javafx.scene.text.Font(123);
+$STAGE.scene = new javafx.scene.Scene(message);
+$STAGE.title = "Hi !";
